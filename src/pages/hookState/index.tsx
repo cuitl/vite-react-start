f08e2898d21841406b5249dfd9c2e37b2accac85
cuitianliang <cuitianliang@hnnewsoft.com>
2022-07-13 10:10:24 +0800

    ci: update @hookstate/core to 4.0.0-rc13,

--- src/pages/hookState/index.tsx
+++ src/pages/hookState/index.tsx
@@ -1,11 +1,11 @@
 /**
  * @file hookstate
  * hookstate 在 React.StrictMode 模式下 locale state 会有问题
+ * 从hookstate 4.0.0-rc12后 支持了React.StrictMode模式 -> https://github.com/avkonst/hookstate/commit/9a2784b729766db48460363210e34fca13a00e73
  * https://github.com/avkonst/hookstate
  * examples: https://github.com/avkonst/hookstate/tree/hookstate-4/docs/index/src/examples
  */
 import { createState, useHookstate } from '@hookstate/core'
-import { useEffect, useId, useRef, useState } from 'react'
 
 const globalState = createState(0)
 setInterval(() => globalState.set(p => p + 1), 3000)
@@ -43,25 +43,17 @@ const Counter = () => {
   )
 }
 
-const ExampleComponent = () => {
-  const state = useHookstate(0)
-  return (
-    <>
-      <b>Counter value: {state.get()} </b>
-      <button onClick={() => state.set(p => p + 1)}>Increment</button>
-    </>
-  )
-}
-
 export default function HookStatePage() {
   return (
     <div>
+      <h2>Global State</h2>
+      <GlobalStateComp />
       <GlobalStateComp />
       <hr className="divide-y my-4" />
+
+      <h2>Local State</h2>
       <Counter />
       <hr className="divide-y my-4" />
-
-      <ExampleComponent />
     </div>
   )
 }
